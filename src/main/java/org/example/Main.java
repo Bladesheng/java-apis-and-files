c91fe6cd903bf7687792fe29cb76b994d95f3c1c
Bladesheng <keadr23@gmail.com>
2024-01-23 15:52:24 +0100

    show top 5 stream by views

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,11 +3,13 @@ package org.example;
 import org.example.models.VideoData;
 import org.example.models.VideosData;
 
+import java.text.NumberFormat;
 import java.time.Duration;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -41,7 +43,18 @@ public class Main {
                 .reduce(Duration.ZERO, Duration::plus)
                 .dividedBy(vods.size());
 
+        List<VOD> vodsSortViews = vods.stream()
+                .sorted(Comparator.comparingInt(VOD::getViewCount).reversed())
+                .limit(5) // top 5 streams
+                .toList();
+
         System.out.println("Number of vods: " + videosData.getData().length);
         System.out.printf("Avg stream duration: %d hours %d minutes%n", avg.toHoursPart(), avg.toMinutesPart());
+
+        System.out.println("Top 3 streams:");
+        for (VOD vod : vodsSortViews) {
+            String viewCount = NumberFormat.getInstance().format(vod.getViewCount());
+            System.out.printf("%s (%s views) (%s) (%s)%n", vod.getTitle(), viewCount, vod.getUrl(), vod.getTime());
+        }
     }
 }
